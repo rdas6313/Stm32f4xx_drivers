749ccb4edba7b638ab6fc469ed6c2b080f13c38e
Raja <rdas6313@gmail.com>
2026-03-31 13:16:10 +0530

    Added FreeRtos

---
+++ Src/interrupt_handlers.c
@@ -0,0 +1,72 @@
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+  void NMI_Handler(void)
+  {
+    
+    while (1)
+    {
+    }
+   
+  }
+  
+  /**
+    * @brief This function handles Hard fault interrupt.
+    */
+  void HardFault_Handler(void)
+  {
+    while (1)
+    {
+  
+    }
+  }
+  
+  /**
+    * @brief This function handles Memory management fault.
+    */
+  void MemManage_Handler(void)
+  {
+
+    while (1)
+    {
+     
+    }
+  }
+  
+  /**
+    * @brief This function handles Pre-fetch fault, memory access fault.
+    */
+  void BusFault_Handler(void)
+  {
+  
+    while (1)
+    {
+    
+    }
+  }
+  
+  /**
+    * @brief This function handles Undefined instruction or illegal state.
+    */
+  void UsageFault_Handler(void)
+  {
+    while (1)
+    {
+     
+    }
+  }
+  
+  /**
+    * @brief This function handles Debug monitor.
+    */
+  void DebugMon_Handler(void)
+  {
+    while (1)
+    {
+     
+    }
+  }
+  
